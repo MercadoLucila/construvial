3f6396ccfb0769c82e7cfb0a57da38db3ee662aa
MercadoLucila <mercadoruizlucila@hotmail.com>
2025-05-15 18:50:18 -0300

    Subida a la bd las primeras tablas Users y Roles

--- database/migrations/0001_01_00_000000_create_roles_table.php
+++ database/migrations/0001_01_00_000000_create_roles_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('rols', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('rols');
+        Schema::dropIfExists('roles');
     }
 };
