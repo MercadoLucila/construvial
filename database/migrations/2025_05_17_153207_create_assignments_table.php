f7002433f38be7901c457e411e023188a706926b
MercadoLucila <mercadoruizlucila@hotmail.com>
2025-05-19 16:51:25 -0300

    intento de arreglar las migraciones, guardado en caso de que no funcione

--- database/migrations/2025_05_17_153207_create_assignments_table.php
+++ database/migrations/2025_05_17_153207_create_assignments_table.php
@@ -28,6 +28,10 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('assigments', function (Blueprint $table) {
+        $table->dropForeign(['machine_id']);
+        });
+
         Schema::dropIfExists('assignments');
     }
 };
